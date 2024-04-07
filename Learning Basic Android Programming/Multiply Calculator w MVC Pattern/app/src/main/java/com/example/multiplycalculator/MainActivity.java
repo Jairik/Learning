1f2944c1fbd55b5fd1ee8732b2b5ee203908007f
Jairik <jairikmc@gmail.com>
2024-04-07 17:52:18 -0400

    Uploading Calculator App
    Completed the App, will be uploading demo outputs for Hello World, Beer Finder, and this commit later.

---
+++ Learning Basic Android Programming/Multiply Calculator w MVC Pattern/app/src/main/java/com/example/multiplycalculator/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.multiplycalculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+/* This file acts as the Controller to the MVC Pattern, communicating between the GUI
+and the model class */
+
+public class MainActivity extends AppCompatActivity {
+
+    Model model = new Model(); //Creating Model Object
+    String reset = "1"; //The initial value when clearing the calculator
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    /* I am experiencing an exception somewhere in here that is crashing app */
+    public void OnClickMultiply(View view) {
+        //Getting the bottom total text to later modify
+        TextView totalTextBox = (TextView) findViewById(R.id.total);
+        //Retrieving the current value of the input box as a String
+        EditText input = (EditText) findViewById(R.id.editTextNumber);
+        String inputText = input.getText().toString();
+
+        //Converting the string inputText into an int
+        int inputValue;
+        try {
+            inputValue = Integer.parseInt(inputText);
+        } catch (NumberFormatException e) { //catching if the text is null
+            inputValue = 1;
+        }
+        //Getting the total from the model
+        int total = model.multiply(inputValue);
+
+        //Converting total to a String to set text
+        String totalString = Integer.toString(total);
+
+        //Setting the value of totalTextBox to total
+        totalTextBox.setText(totalString); //setting as inputText for testing
+    }
+
+    public void OnClickClear(View view) {
+        //Getting the bottom total text to later modify
+        TextView totalTextBox = (TextView) findViewById(R.id.total);
+
+        //Clear the total from the model
+        model.clear();
+
+        //Set the total in the GUI to 1 (the new total)
+        totalTextBox.setText(reset);
+
+    }
+}
---
+++ Learning Basic Android Programming/Multiply Calculator w MVC Pattern/app/src/main/java/com/example/multiplycalculator/Model.java
@@ -0,0 +1,25 @@
+package com.example.multiplycalculator;
+
+/* Acts as the model in the MVC Design Pattern, holding relevant
+data and logic for calculations */
+public class Model {
+    int total; //Holds the current result
+
+    //Default Constructor- initializes the total as 1
+    public Model() {
+        total = 1;
+    }
+
+    //Multiplies the current total by the parameter int and returns the result
+    public int multiply(int num) {
+        total = total*num;
+        return total;
+    }
+
+    //Clears the current total
+    public void clear() {
+        total = 1;
+    }
+
+
+}
