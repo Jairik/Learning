111ba4e7ea8ebec38a7a142d2eac4f7d66fadb7d
Jairik <jairikmc@gmail.com>
2024-04-15 13:56:12 -0400

    Finishing simple calculator app
    Adding some more touches to it, it is still bugging out when the equals button is pressed so I'll need to smooth that out.

--- Android Programming/More Intermediate Exercises/Simple_Calculator/app/src/main/java/com/example/simple_calculator/MainActivity.java
+++ Android Programming/More Intermediate Exercises/Simple_Calculator/app/src/main/java/com/example/simple_calculator/MainActivity.java
@@ -4,21 +4,87 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import android.app.Activity;
+
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+/* Author: JJ McCauley
+ * Date of Last Update: 4/15/24
+ * Interacts with the view and SimpleExpression class to make a simple calculator */
 public class MainActivity extends AppCompatActivity {
+
+    private TextView numberDisplay;
+    private SimpleExpression expression;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        }); */
+        //Getting the number display and making a SimpleExpression object
+        numberDisplay = (TextView) findViewById(R.id.output);
+        expression = new SimpleExpression();
     }
+
+
+    public void clear(View view) {
+        expression.clearAll();
+        numberDisplay.setText("0");
+    }
+
+    //Handling operands (number buttons)
+    public void operand(View view) {
+        String val = (String) numberDisplay.getText();
+        String digit = (String) view.getContentDescription();
+        if (val.charAt(0) == '0') {
+            numberDisplay.setText(digit);
+        }
+        else {
+            numberDisplay.setText((String) numberDisplay.getText() + digit.charAt(0));
+        }
+    }
+
+    //handling operators
+    public void operator(View view) {
+        String operator = (String) view.getContentDescription();
+        try {
+            String val = (String) numberDisplay.getText();
+            expression.setOperand1((int) Integer.parseInt(val.toString()));
+        }
+        catch(NumberFormatException e) {
+            expression.setOperand1(0);
+        }
+        numberDisplay.setText("0");
+        expression.setOperator(operator);
+    }
+
+    //Handles the event that the = button is pushed, calculates total and updates output
+    public void computeTotal(View view) {
+        try {
+            String val = (String) numberDisplay.getText();
+            expression.setOperand2((int) Integer.parseInt(val.toString()));
+        }
+        catch (NumberFormatException e) {
+            expression.setOperand2(0);
+        }
+        numberDisplay.setText((expression.getEndValue()));
+    }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu item) {
+        //Inflates the menu
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    } */
 }
---
+++ Android Programming/More Intermediate Exercises/Simple_Calculator/app/src/main/java/com/example/simple_calculator/SimpleExpression.java
@@ -0,0 +1,77 @@
+package com.example.simple_calculator;
+
+/* Author: JJ McCauley
+*  Date: 4/15/24
+* This class acts like that of the controller, holding relevant values and performing
+* the calculation logic */
+
+public class SimpleExpression {
+    private int operand1; //The first number typed into the calculator
+    private int operand2; //The second number typed into the calculator
+    private String operator; //The operator (+ - x /)
+    private int endValue; //The value of operand1 <operator> operand2
+
+    //Default constructor that sets all the values to 0
+    public SimpleExpression() {
+        operand1 = 0;
+        operand2 = 0;
+        operator = "+";
+        endValue = 0;
+    }
+
+    /* ----- Getters & Setters for the different values ----- */
+    public void setOperand1(int value) {
+        operand1 = value;
+    }
+
+    public int getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand2(int value) {
+        operand2 = value;
+    }
+
+    public int getOperand2() {
+        return operand2;
+    }
+
+    public void setOperator(String value) {
+        operator = value;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public int getEndValue() {
+        calculate(); //Helper function that will change the value of endOperator
+        return endValue;
+    }
+
+    /* Apply the correct operation to appropriately change the endValue */
+    public void calculate() {
+        endValue = 0; //Resetting
+        if(operator.equals("+")) {
+            endValue = operand1 + operand2;
+        }
+        else if(operator.equals("-")) {
+            endValue = operand1 - operand2;
+        }
+        else if(operator.equals("x")) {
+            endValue = operand1 * operand2;
+        }
+        else if(operator.equals("/")) {
+            endValue = operand1 / operand2;
+        }
+        else { //operator = %
+            endValue = operand1 % operand2;
+        }
+    }
+
+    /* Clear the operands */
+    public void clearAll() {
+        operand1 = 0;
+        operand2 = 0;
+    }
+}
