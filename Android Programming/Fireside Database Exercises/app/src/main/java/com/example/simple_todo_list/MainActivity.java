66f61a6f2a6c6c264196e584dda62165ae3880d5
Jairik <jairikmc@gmail.com>
2024-04-21 15:35:17 -0400

    Simple To-Do Initial Commit
    Not quite finished but added the basic functions

---
+++ Android Programming/Fireside Database Exercises/app/src/main/java/com/example/simple_todo_list/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.simple_todo_list;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+/* Main Program - JJ's To-Do List */
+
+public class MainActivity extends AppCompatActivity {
+    //Member variables
+    ArrayList<String> items; //Will hold the tasks/items
+    ArrayAdapter<String> itemsAdapter; //Simplifies displaying the items contents
+    ListView lvItems; //Where items will be displayed
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //Setting up variables
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+        //Invoking ListViewListener to delete items
+        setUpListViewListener();
+    }
+
+    //OnClick method for the "add item" button
+    public void onAddItem(View view) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+
+    //
+    private void setUpListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+}
