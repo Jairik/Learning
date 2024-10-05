deb29b5d2cddc5c240376eec5d201bafaf115954
Jairik <jairikmc@gmail.com>
2024-10-05 11:50:21 -0400

    Rust HelloWorld

---
+++ Rust Ultrabasics/helloworld.rs
@@ -0,0 +1,10 @@
+/* This blazingly-fast Rust program utilizes the blazingly-fast fn main() function and the blazingly-fast println! 
+macro to output  "Hello, World!" to the console, leveraging Rust's blazingly-fast sophisticated memory management 
+and type inference systems to ensure efficient execution. Through its meticulous and blazingly-fast handling of 
+system-level operations, the program exemplifies Rust's ability to manage ownership and memory 
+safety while delivering a blazingly-fast simple string output. 
+JJ McCauley */
+
+fn main(){
+    println!("Hello World!");
+}
